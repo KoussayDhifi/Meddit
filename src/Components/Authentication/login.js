517c9ffc1708edfd9ea0a7dd0a45bcc7bc22acec
Koussay <koussaydhifi48@gmail.com>
2021-09-19 17:27:12 +0100

    Add default and Upload pfp functionality

--- src/Components/Authentication/login.js
+++ src/Components/Authentication/login.js
@@ -10,9 +10,9 @@ const Login = () => {
             </div>
             <br/>
             <label for="email">Email:</label><br/>
-            <input type="email" id="email" className="email"/><br/>
+            <input type="email" id="email" className="email" required/><br/>
             <label for="password">Password:</label><br/>
-            <input type="password" id="password" className="password"/><br/>
+            <input type="password" id="password" className="password" required/><br/>
             <input type="checkbox" id="remember" className="remember"/><label class="rememberlabel" for="remember"><p>Remember Me</p></label>
             <br/>
             <input type="submit" value="Login!"/>
--- src/Components/Registeration/register.js
+++ src/Components/Registeration/register.js
@@ -3,6 +3,28 @@ import {Link} from 'react-router-dom'
 import {useState} from 'react';
 
 const Register = () => {
+  //states
+  const [style,setStyle] = useState({display:'none'})
+  const [checkedDefault,setCheckedDefault] = useState('checked')
+  const [checkedUpload,setCheckedUpload] = useState('')
+
+  //Functions for handling
+  const handleChangeRadio = () => {
+    if (document.getElementById("upload").checked)
+    {
+      setStyle({display:'flex'})
+      setCheckedUpload('checked')
+      setCheckedDefault('')
+
+    }else if (document.getElementById("default").checked)
+    {
+        setStyle({display:'none'})
+        setCheckedUpload('')
+        setCheckedDefault('checked')
+
+    }
+  }
+
   return(
     <div className="Register">
       <form>
@@ -10,20 +32,21 @@ const Register = () => {
           <h1><Link to="/login"><a>Login</a></Link>|<Link to="/register"><a>Register</a></Link></h1>
         </div>
         <label for="name">Name:</label><br/>
-        <input type="text" id="name" className="name"/><br/>
+        <input type="text" id="name" className="name" placeHolder="Name"/><br/>
         <label for="email">Email:</label><br/>
-        <input type="email" id="email" className="email"/><br/>
+        <input type="email" id="email" className="email" placeHolder="Email"/><br/>
         <label for="p1">Passowrd:</label><br/>
-        <input type="password" id="p1" className="p1"/><br/>
+        <input type="password" id="p1" className="p1" placeHolder="Password"/><br/>
         <label for="p2">Confirm Password:</label><br/>
-        <input type="password" id="p2" className="p2"/><br/>
+        <input type="password" id="p2" className="p2" placeHolder="Confirm Password"/><br/>
         <label for="date">Date of birth:</label><br/>
         <input type="date" id="date" className="date"/><br/>
         <label for="date">Profile Pic:</label><br/>
         <img src="/Pfps/default.jpg"/><br/>
-        <input type="radio" id="default" name='pfp' checked/><label class="default" for="default"><p>Default</p></label>
-        <input type="radio" id="upload" name='pfp'/><label class="upload" for="upload"><p>Upload</p></label>
-        <input type="file"/>
+        <input type="radio" id="default" name='pfp' checked={checkedDefault} onChange={handleChangeRadio}  /><label class="default" for="default"><p>Default</p></label>
+        <input type="radio" id="upload" name='pfp' checked={checkedUpload} onChange={handleChangeRadio}/><label class="upload" for="upload"><p>Upload</p></label><br/>
+        <input type="file" id="file" style={style}/><br/>
+        <input type="submit" value="Create an account!"/>
 
 
       </form>
