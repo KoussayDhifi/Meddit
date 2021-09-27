5b03b856f1541b3138bbf8b2f1edde9e41addbcf
Koussay <koussaydhifi48@gmail.com>
2021-09-27 15:35:45 +0100

    Add states to register and login pages now trying to add eye for password showing

--- src/Components/Authentication/login.js
+++ src/Components/Authentication/login.js
@@ -1,7 +1,23 @@
 import './login.css'
 import {Link} from 'react-router-dom'
+import {useState} from 'react'
 
 const Login = () => {
+
+    //States
+    const [EmailField,setEmailField] = useState('')
+    const [PasswordField,setPasswordField] = useState('')
+
+
+    //Functions for handling
+
+    const handleEmailField = (e) => {
+      setEmailField(e.target.value);
+    }
+
+    const handlePasswordField = (e) => {
+      setPasswordField(e.target.value);
+    }
       return (
         <div className="Login">
           <form>
@@ -10,9 +26,9 @@ const Login = () => {
             </div>
             <br/>
             <label for="email">Email:</label><br/>
-            <input type="email" id="email" className="email" required/><br/>
+            <input type="email" id="email" className="email" required value={EmailField} onChange={handleEmailField}/><br/>
             <label for="password">Password:</label><br/>
-            <input type="password" id="password" className="password" required/><br/>
+            <input type="password" id="password" className="password" required value={PasswordField} onChange={handlePasswordField}/><img src="/eye/view.png" className="eye"/><br/>
             <input type="checkbox" id="remember" className="remember"/><label class="rememberlabel" for="remember"><p>Remember Me</p></label>
             <br/>
             <input type="submit" value="Login!"/>
--- src/Components/Registeration/register.js
+++ src/Components/Registeration/register.js
@@ -7,6 +7,11 @@ const Register = () => {
   const [style,setStyle] = useState({display:'none'})
   const [checkedDefault,setCheckedDefault] = useState('checked')
   const [checkedUpload,setCheckedUpload] = useState('')
+  const [nameField,setNameField] = useState('')
+  const [emailField,setEmailField] = useState('')
+  const [passwordField,setPasswordField] = useState('')
+  const [conPasswordField,setConPasswordField] = useState('')
+  const [dateField,setDateField] = useState("yyyy-mm-dd")
 
   //Functions for handling
   const handleChangeRadio = () => {
@@ -25,6 +30,26 @@ const Register = () => {
     }
   }
 
+  const handleNameChange = (e) => {
+    setNameField(e.target.value);
+  }
+
+  const handleEmailChange = (e) => {
+    setEmailField(e.target.value);
+  }
+
+  const handlePasswordField = (e) => {
+    setPasswordField(e.target.value);
+  }
+
+  const handleConPasswordField = (e) => {
+    setConPasswordField(e.target.value);
+  }
+
+  const handleDateField = (e) => {
+    setDateField(e.target.value);
+  }
+
   return(
     <div className="Register">
       <form>
@@ -32,15 +57,15 @@ const Register = () => {
           <h1><Link to="/login"><a>Login</a></Link>|<Link to="/register"><a>Register</a></Link></h1>
         </div>
         <label for="name">Name:</label><br/>
-        <input type="text" id="name" className="name" placeHolder="Name"/><br/>
+        <input type="text" id="name" className="name" placeHolder="Name" value={nameField} onChange={handleNameChange}/><br/>
         <label for="email">Email:</label><br/>
-        <input type="email" id="email" className="email" placeHolder="Email"/><br/>
+        <input type="email" id="email" className="email" placeHolder="Email" value={emailField} onChange={handleEmailChange}/><br/>
         <label for="p1">Passowrd:</label><br/>
-        <input type="password" id="p1" className="p1" placeHolder="Password"/><br/>
+        <input type="password" id="p1" className="p1" placeHolder="Password" value={passwordField} onChange={handlePasswordField}/><br/>
         <label for="p2">Confirm Password:</label><br/>
-        <input type="password" id="p2" className="p2" placeHolder="Confirm Password"/><br/>
+        <input type="password" id="p2" className="p2" placeHolder="Confirm Password" value={conPasswordField} onChange={handleConPasswordField}/><br/>
         <label for="date">Date of birth:</label><br/>
-        <input type="date" id="date" className="date"/><br/>
+        <input type="date" id="date" className="date" value={dateField} onChange={handleDateField}/><br/>
         <label for="date">Profile Pic:</label><br/>
         <img src="/Pfps/default.jpg"/><br/>
         <input type="radio" id="default" name='pfp' checked={checkedDefault} onChange={handleChangeRadio}  /><label class="default" for="default"><p>Default</p></label>
